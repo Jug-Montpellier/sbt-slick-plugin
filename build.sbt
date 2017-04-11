f69c9b1a09f59fe8d400c68d0985e7ed3499641a
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2017-04-11 22:41:16 +0200

    More FP
    IMHO :)

--- build.sbt
+++ build.sbt
@@ -4,14 +4,9 @@ organization := "org.jug-montpellier"
 
 resolvers += Resolver.file("local", file(Path.userHome.absolutePath + "/.ivy2/local"))(Resolver.ivyStylePatterns)
 
-//scalaVersion := "2.12.1"
-//scalaVersion := "2.11.8"
-
 sbtPlugin := true
 
-val slickVersion = "3.2.0"
-
-//libraryDependencies += "com.typesafe.slick" %% "slick-codegen" % slickVersion
+libraryDependencies += "com.typesafe" % "config" % "1.3.1"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1"
 
--- src/main/scala/org/jugmontpellier/slickcodegen/SlickCodegenPlugin.scala
+++ src/main/scala/org/jugmontpellier/slickcodegen/SlickCodegenPlugin.scala
@@ -1,26 +1,55 @@
 package org.jugmontpellier.slickcodegen
 
+import java.nio.file.{Files, Paths}
+
+import com.typesafe.config.ConfigFactory
 import sbt._
 import Keys._
+
+import scala.util.Try
 
 object SlickCodegenPlugin extends AutoPlugin {
 
   override def requires: Plugins = plugins.JvmPlugin
 
+  object autoImport {
+    val slickCodegenConfFile = settingKey[String]("slick codegen configuration file path")
+    val slickCodegenConf = settingKey[String]("slick codegen conf ( #thistoken )")
+  }
+
+  import autoImport._
+
   override lazy val projectSettings = Seq(
+    slickCodegenConfFile := "src/main/slick/slick-codegen.conf",
+    runner := new ForkRun(ForkOptions()),
     sourceGenerators in Compile += Def.task[Seq[File]] {
-      generate( sourceManaged.value,
-        (dependencyClasspath in Compile).value, runner.value, streams.value
-      )}.taskValue
-      //(sourceManaged, dependencyClasspath in Compile, runner in Compile, streams) map { case (dir, cp, r, s) => generate(dir)
-      
+      val dir = (sourceManaged in Compile).value
+      val cp = (dependencyClasspath in Compile).value
+      val confFile = slickCodegenConfFile.value
+      val conf = slickCodegenConf.value
+      implicit val logger = streams.value.log
+      generate(dir, cp, runner.value, confFile, conf).toSeq
+    }.taskValue
+
   )
 
-  def generate(dir: File,cp: Classpath, run: ScalaRun, s: TaskStreams) : Seq[File] = {
-    val outputDir = dir // place generated files in sbt's managed sources folder
-    val conf = "file:src/main/slick/slick-codegen.conf#kafka"
-    toError(run.run("slick.codegen.SourceCodeGenerator", cp.files, Array(conf, outputDir.getPath), s.log))
-    Seq(dir / "kafka" / "model" / "Tables.scala")
+  def generate(outputDir: File, cp: Classpath, runner: ScalaRun, confFile: String, conf: String)(implicit logger: Logger): Option[File] = for {
+      confPath <- confPath(confFile)
+      config <- Try(ConfigFactory.parseFile(confPath.toFile).getConfig(conf)).toOption
+    } yield {
+      toError(runner.run("slick.codegen.SourceCodeGenerator", cp.files, Array(s"file:$confFile#$conf", outputDir.getPath), logger))
+      outputDir / config.getString("codegen.package").replace('.', '/') / "Tables.scala"
+    }
+
+  private def confPath(confFile: String)(implicit logger: Logger) = Option(Paths.get(confFile)) map {
+    case p if p.isAbsolute => p
+    case p => Paths.get(sys.props("user.dir")).resolve(p)
+  } flatMap {
+    case p if Files.exists(p) => Some(p)
+    case p =>
+      logger.error(s"$p does not exist!")
+      None
   }
+
 
 }
